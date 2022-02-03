a01c65c554e04c5112632865eae76824660fafeb
mouragama <mouragama@gmail.com>
2022-02-03 17:37:17 -0400

    commit update

--- categoryControllers/categoryControllers.js
+++ categoryControllers/categoryControllers.js
@@ -3,6 +3,19 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/admin/categories',(req, res) =>{
+    res.render('admin/categories/new');
+})
 
+router.post('/categories/dinheiro',(req, res) =>{
+    // let name = req.body.nome;
+    // let money = req.body.num;
+
+    res.send(`<h1>Ainda Bem que foi um teste!!! </br></br>
+        Parabens vc foi enganado kkkk...
+    `)
+
+    
+})
 
 module.exports = router;
--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 // ? CONTROLLERS
 const categoriesControllers = require('./categoryControllers/categoryControllers');
 app.use('/',categoriesControllers);
 
-const PORT = process.env.PORT || 3000;
+// ? Rota Comum
+app.listen(8080, () =>{console.log('teste de servidor!!!')})
 
-app.listen(PORT, () =>{
-    console.log('servidor iniciado');
-});
+// * Rota Heroku
+// const PORT = process.env.PORT || 3000;
+
+// app.listen(PORT, () =>{
+//     console.log('servidor iniciado');
+// });
 
 // * DEPENDECIAS
 app.set('view engine',('ejs'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 app.get('/',(req, res) =>{
     res.render('index');
