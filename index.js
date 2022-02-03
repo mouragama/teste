46a8dcc53b6dfe4b47c86d0b18cb867cbce8c645
mouragama <mouragama@gmail.com>
2022-02-03 17:38:15 -0400

    commit update

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const categoriesControllers = require('./categoryControllers/categoryControllers
 app.use('/',categoriesControllers);
 
 // ? Rota Comum
-app.listen(8080, () =>{console.log('teste de servidor!!!')})
+// app.listen(8080, () =>{console.log('teste de servidor!!!')})
 
 // * Rota Heroku
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-// app.listen(PORT, () =>{
-//     console.log('servidor iniciado');
-// });
+app.listen(PORT, () =>{
+    console.log('servidor iniciado');
+});
 
 // * DEPENDECIAS
 app.set('view engine',('ejs'));
